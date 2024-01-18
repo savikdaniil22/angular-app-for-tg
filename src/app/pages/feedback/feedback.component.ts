fba032704cbba6352e34bdda305b8d2ab50edbb6
Daniil Savik <savikdaniil22@gmail.com>
2024-01-19 02:09:09 +0300

    first commit

---
+++ src/app/pages/feedback/feedback.component.ts
@@ -0,0 +1,54 @@
+import { TelegramService } from './../../services/telegram.service';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  standalone: true,
+  imports: [],
+  styles: `
+  .form{
+    height: 70vh;
+    justify-content: center;
+  }
+  `,
+  template: `<form class="centered form">
+    <h2 class="mv">Обратная связь</h2>
+    <textarea
+      [value]="feedback()"
+      (input)="handleChange($event)"
+      class="form-control"
+    ></textarea>
+  </form>`,
+})
+export class FeedbackComponent implements OnInit, OnDestroy {
+  feedback = signal('');
+
+  constructor(private telegram: TelegramService) {
+    this.sendData = this.sendData.bind(this);
+  }
+
+  ngOnInit(): void {
+    this.telegram.MainButton.setText('Отправить сообщение');
+    // this.telegram.MainButton.show();
+    // this.telegram.MainButton.disable();
+    this.telegram.MainButton.hide();
+    this.telegram.MainButton.onClick(this.sendData);
+  }
+
+  sendData() {
+    this.telegram.sendData({ feedback: this.feedback() });
+  }
+
+  handleChange(event) {
+    this.feedback.set(event.target.value);
+    if (this.feedback().trim()) {
+      this.telegram.MainButton.show();
+    } else {
+      this.telegram.MainButton.hide();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.telegram.MainButton.offClick(this.sendData);
+  }
+}
